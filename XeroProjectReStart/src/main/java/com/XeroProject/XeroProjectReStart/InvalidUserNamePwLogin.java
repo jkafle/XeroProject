dce5d5394df71c074fa0d6320133876cb7254326
jayakafle <jayakafle@jayas-air.attlocal.net>
2019-04-05 16:00:10 -0500

    Updated first TestCase!

--- XeroProjectReStart/src/main/java/com/XeroProject/XeroProjectReStart/InvalidUserNamePwLogin.java
+++ XeroProjectReStart/src/main/java/com/XeroProject/XeroProjectReStart/InvalidUserNamePwLogin.java
@@ -1,15 +1,27 @@
 package com.XeroProject.XeroProjectReStart;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class InvalidUserNamePwLogin {
+public class InvalidUserNamePwLogin extends XeroReusable {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		System.setProperty("webdriver.chrome.driver", "/Users/jayakafle/Downloads/chromedriver");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.xero.com/us/");
+		driver.get("https://login.xero.com/us/");
+		
+		WebElement emailAddress= driver.findElement(By.xpath("//input[contains(@placeholder,'Email address')]"));
+		textEnter(emailAddress, "user@gmail.com");
+		
+		WebElement pw = driver.findElement(By.xpath("//input[contains(@placeholder,'Password')]"));
+		textEnter(pw, "pwd");
+		
+		WebElement clickObj = driver.findElement(By.xpath("//button[@id='submitButton']"));
+		clickAction(clickObj);
+		
 	}
 
 }
--- XeroProjectReStart/src/main/java/com/XeroProject/XeroProjectReStart/XeroReusable.java
+++ XeroProjectReStart/src/main/java/com/XeroProject/XeroProjectReStart/XeroReusable.java
@@ -1,9 +1,22 @@
 package com.XeroProject.XeroProjectReStart;
 
+import org.openqa.selenium.WebElement;
+
 public class XeroReusable {
 
-	public static void textEnter() {
+	public static void textEnter(WebElement obj, String value) throws InterruptedException {
 		
-		
+		if(obj.isDisplayed()) {
+		Thread.sleep(3000);	
+		obj.sendKeys(value);
+		System.out.println("The value is successfully entered!");
+		}
+		else {
+			System.out.println("The field does not exist!");
+		}
+	}
+	public static void clickAction(WebElement obj) throws InterruptedException {
+		Thread.sleep(3000);
+		obj.click();
 	}
 }
